71568c74e2bbb2bb4f63c20a48aa6455c9a32eaf
Danny Garcia <dannygarciaDEV@gmail.com>
2025-09-29 12:51:50 -0400

    best time to buy and sell stock

---
+++ week4/Best Time to Buy and Sell Stock.js
@@ -0,0 +1,43 @@
+// Best Time to Buy and Sell Stock
+// You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+
+// You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+
+// Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+// Example 1:
+
+// Input: prices = [10,1,5,6,7,1]
+
+// Output: 6
+// Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+
+// Example 2:
+
+// Input: prices = [10,8,7,5,2]
+
+// Output: 0
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let minPrice = Infinity;
+        let maxProfit = 0;
+
+        for (let price of prices) {
+            // Update min price if current price is lower
+            if (price < minPrice) {
+                minPrice = price;
+            }
+            // Calculate profit if we sold today
+            let profit = price - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+}
